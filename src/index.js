0ec91d09ebc9b65f3ed39f2f72153f33d39ec75d
Nour Adel <noura7305@gmail.com>
2019-11-04 14:12:13 +0200

    constructor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component{
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
-            posision => console.log(posision),
-            (err) => console.log(err)
-        ) 
-           
-        return <div> hiii</div>
-   } 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+        posision => {this.setState({lat: posision.coords.latitude});},
+        err => console.log(err)
+      );
+  
+  }
+  render() {
+ 
+    return <div> latitude: {this.state.lat}</div>;
+  }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
